cf87e72e0f9c35858afe2610bcac1823cefed60f
Kevin Fox <yanelegomego24@yandex.ru>
2019-11-16 23:38:35 +0300

    refactor views.py, add ContentManager logic to handler

--- content_downloader/app/views.py
+++ content_downloader/app/views.py
@@ -1,7 +1,6 @@
 from aiohttp import web
-from .serializers.request import content_2ch_request_schema
+from .serializers.request import content_2ch_request_validate_schema, content_2ch_thread_object_schema
 from content_downloader.ext.for_2ch.downloader import Downloader
-from content_downloader.ext.zipper import zipify_content, clean_folder
 
 routes = web.RouteTableDef()
 
@@ -11,11 +10,21 @@ async def hello(request):
     return web.json_response({'health': 'ok'})
 
 
-@routes.get('/content/download')
-async def hello(request):
-    url = content_2ch_request_schema.load(request.query)['threadUrl']
-    downloader = Downloader(tread_url=url)
-    path = await downloader.download_content_from_thread()
-    path_to_zip = zipify_content(path)
-    clean_folder(path)
-    return web.FileResponse(path=path_to_zip)
+@routes.get('/content/download/2ch')
+async def download_content_2ch(request):
+    thread_url = content_2ch_request_validate_schema.load(request.query)
+    tread_obj = content_2ch_thread_object_schema.dump(thread_url)
+    dir_name = tread_obj['thread_name']
+
+    content_path = request.app.cm.create_and_get_content_dir(dir_name=dir_name)
+
+    downloader = Downloader(tread_url=tread_obj['thread_url_json'], path_to_downloaded_content=content_path)
+    await downloader.download_content_from_thread()
+
+    zip_path = request.app.cm.zipify_content(path_from=content_path, zip_name=dir_name)
+
+    request.app.cm.delete_content_dir(dir_name)
+
+    header = {'Content-Disposition': f'attachment; filename="{dir_name}"'}
+    del downloader
+    return web.FileResponse(path=zip_path, headers=header)
